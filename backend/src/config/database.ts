ab2d69dd04d704b3e75de49dc31b2a1ed566915a
Mario Roberto Molano <mariormolano@yahoo.com>
2024-10-05 00:11:21 -0500

    nuevo backend

---
+++ backend/src/config/database.ts
@@ -0,0 +1,16 @@
+import { DataSource } from "typeorm";
+import { User } from "../features/user/userEntity";
+require("dotenv").config();
+
+const con = new DataSource({
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  entities: [User],
+});
+
+export default con;
---
+++ backend/src/config/swagger.ts
@@ -0,0 +1,25 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+// Definir opciones para Swagger
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Developer Social Network API",
+      version: "1.0.0",
+      description: "Documentación de la API usando Swagger",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000", // URL base de tu API
+      },
+    ],
+  },
+  apis: ["./src/features/**/*.ts"], // Ruta a todas las subcarpetas dentro de src/features
+};
+
+// Generar la especificación de Swagger
+const swaggerSpecs = swaggerJsdoc(swaggerOptions);
+
+module.exports = { swaggerUi, swaggerSpecs };
