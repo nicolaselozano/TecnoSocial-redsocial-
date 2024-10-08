ea5a1b687ccb4f71223037e59e3baa190e7f3c3b
SantiagoZarate <santiago2001zarate@gmail.com>
2024-10-07 22:55:04 -0300

    docs: move swagger docs to docs directory

--- backend/src/config/swagger.ts
+++ backend/src/config/swagger.ts
@@ -1,28 +1,9 @@
-import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+import yaml from "yamljs";
+import path from "node:path";
 
-// Definir opciones para Swagger
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Developer Social Network API",
-      version: "1.0.0",
-      description: "Documentación de la API usando Swagger",
-    },
-    servers: [
-      {
-        url: "http://ec2-100-25-217-101.compute-1.amazonaws.com",
-      },
-    ],
-  },
-  apis: [
-    "./src/features/**/postRoutes.ts",
-    "./src/features/**/routes/userRoutes.ts",
-  ],
-};
-
-// Generar la especificación de Swagger
-const swaggerSpecs = swaggerJsdoc(swaggerOptions);
+const swaggerSpecs = yaml.load(
+  path.resolve(__dirname, "../docs/build/swagger.yaml")
+);
 
 export { swaggerUi, swaggerSpecs };
