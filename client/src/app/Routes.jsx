2b2741e2ee493353b93c8eea6f5d77d45660547f
Nicolas <103128953+nicolaselozano@users.noreply.github.com>
2024-10-09 22:19:17 -0300

    Routes Button import de mas.jsx

--- client/src/app/Routes.jsx
+++ client/src/app/Routes.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import RedirectRegister from '../components/Auth/Register/RedirectRegister';
 import RedirectLogin from '../components/Auth/Login/RedirectLogin';
-import Button from '../components/UI/Button/Button';
 const AllRoutes = () => {
   return (
     <Routes>
