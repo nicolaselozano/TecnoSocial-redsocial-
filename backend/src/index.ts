e1075411a1187c605dc2cc083c321c0b2eea4971
SantiagoZarate <santiago2001zarate@gmail.com>
2024-10-16 13:49:37 -0300

    fix: update docs endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import envs from './config/envs';
 import { swaggerSpecs, swaggerUi } from './config/swagger';
 import authUserRoutes from './features/auth_user/routes/authUserRoutes';
 import imageRouter from './features/image/imageRoutes';
+import likeRouter from './features/like/likeRoutes';
 import postRouter from './features/post/postRoutes';
 import projectRouter from './features/project/projectRoutes';
 import socialNetworksRouter from './features/social_networks/socialNetworksRoutes';
@@ -15,15 +16,14 @@ import { globalErrors } from './middlewares/GlobalErrors';
 import { setBaseMiddlewares } from './middlewares/SetBaseMiddlewares';
 import { healthcheck } from './utils/healthcheck';
 import { redirectToDocs } from './utils/redirectToDocs';
-import likeRouter from './features/like/likeRoutes';
 
 const app = express();
 
 setBaseMiddlewares(app);
 
-app.get('/api/', redirectToDocs);
+app.get('/api', redirectToDocs);
 app.use('/api/health', healthcheck);
-app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 app.use(
   '/api/v1',
   userRouter,
@@ -44,7 +44,7 @@ con
     console.log('Conexión a la base de datos exitosa');
     app.listen(envs.PORT, () => {
       console.log(` Servidor corriendo en ${envs.URL}:${envs.PORT}`);
-      console.log(` Documentación disponible en ${envs.URL}:${envs.PORT}/api-docs`);
+      console.log(` Documentación disponible en ${envs.URL}:${envs.PORT}/api/docs`);
     });
   })
   .catch((err) => {
