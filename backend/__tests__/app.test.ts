6c69cdf75d94d81dc079f294be766a4e825b8a1d
SantiagoZarate <santiago2001zarate@gmail.com>
2024-10-18 23:38:10 -0300

    chore: setup supertest for integration tests

--- backend/__tests__/app.test.ts
+++ backend/__tests__/app.test.ts
@@ -1,3 +1,9 @@
-test('Testing', () => {
+import { app } from '@/app';
+import supertest from 'supertest';
+
+const request = supertest(app);
+
+test('GET /health', async () => {
+  await request.get('/api/health').expect(200);
   expect(2).toBe(2);
 });
--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,15 +1,20 @@
 import type { Config } from 'jest';
 
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
 const config: Config = {
   testEnvironment: 'node',
   transform: {
     '^.+.tsx?$': ['ts-jest', {}],
   },
   preset: 'ts-jest',
-  collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  // collectCoverage: true,
 };
 
 export default config;
