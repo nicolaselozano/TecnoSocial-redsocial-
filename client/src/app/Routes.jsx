a8fea582726bec43d55e517ca79d731880bcc9b5
Nicolas <103128953+nicolaselozano@users.noreply.github.com>
2024-10-23 10:46:13 -0300

    Update Routes.jsx

--- client/src/app/Routes.jsx
+++ client/src/app/Routes.jsx
@@ -5,11 +5,8 @@ import AuthModal from "../components/AuthModals/AuthModal";
 import { ExplorePage } from "../components/Explore/ExplorePage";
 import Profile from '../layout/Profile';
 import Notification from "../view/Notificate/Notificate";
-<<<<<<< HEAD
 import { NotificationBar } from "../components/Notification_bar/NotificationBar";
-=======
 import SimilarProfilesPage from "../components/Profile/SimilarProfilesPage";
->>>>>>> 18f5bb4fbdd2a035eb4abb2b94ce48dadf2d9f0d
 
 const AllRoutes = () => {
   return (
@@ -24,14 +21,10 @@ const AllRoutes = () => {
       {/* Paginas generales */}
       <Route path="/explore" element={<ExplorePage />} />
       <Route path="/notifications" element={<Notification />} />
-<<<<<<< HEAD
-
       <Route path="/notificationsBar" element={<NotificationBar />} />
 
-=======
       {/* Ruta Perfiles Similares */}
       <Route path="/similares" element={<SimilarProfilesPage/>}/>
->>>>>>> 18f5bb4fbdd2a035eb4abb2b94ce48dadf2d9f0d
     </Routes>
   );
 };
