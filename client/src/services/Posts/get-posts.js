f2ddff8ba82d5bdfb967c0153ac3fac5b8dacb8d
jfpanchi <jefpanchi@gmail.com>
2024-10-25 15:28:18 -0500

    fix: :truck: Update conditional get posts
    Update conditional get posts service

--- client/src/services/Posts/get-posts.js
+++ client/src/services/Posts/get-posts.js
@@ -1,24 +1,28 @@
 import { APIDOMAIN, APIDOMAIN_VERSION } from "../../../vars";
 
 export const getPosts = async (limit, page) => {
+  const user = JSON.parse(localStorage.getItem("userdata"));
+
+  const route = user
+    ? `${APIDOMAIN}${APIDOMAIN_VERSION}/post/me?limit=${limit}&page=${page}`
+    : `${APIDOMAIN}${APIDOMAIN_VERSION}/post?limit=${limit}&page=${page}`;
+
   try {
-    const response = await fetch(
-      `${APIDOMAIN}${APIDOMAIN_VERSION}/post?limit=${limit}&page=${page}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        credentials: 'include',
-      }
-    );
+    const response = await fetch(route, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      credentials: "include",
+    });
 
     if (!response.ok) {
       throw new Error("Error en la respuesta del servidor");
     }
 
     const data = await response.json();
+    console.log(data);
     return data;
   } catch (error) {
     console.error("Error al obtener los posts:", error);
