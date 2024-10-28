b32969f9913161e68099c2bc7dad8de01ae63407
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2024-10-27 22:18:34 -0300

    fix : vars

--- client/vars.js
+++ client/vars.js
@@ -4,11 +4,11 @@ export const SCOPE = 'offline_access';
 export const CLIENT_ID = 'xEbVjJQOj5vvumIFvChbtmAqIPrB2Kgg';
 export const RESPONSE_TYPE = 'code';
 
-export const REDIRECT_URI = 'http://localhost:5173/redirect';
-export const CLIENT_DOMAIN = 'http://localhost:5173/';
-export const APIDOMAIN = 'http://localhost:3000';
+// export const REDIRECT_URI = 'http://localhost:5173/redirect';
+// export const CLIENT_DOMAIN = 'http://localhost:5173/';
+// export const APIDOMAIN = 'http://localhost:3000';
 
-// export const REDIRECT_URI = 'https://client-tecno.web.app/redirect';
-// export const CLIENT_DOMAIN = 'https://client-tecno.web.app';
-// export const APIDOMAIN = 'https://mrmolanodev.site';
+export const REDIRECT_URI = 'https://client-tecno.web.app/redirect';
+export const CLIENT_DOMAIN = 'https://client-tecno.web.app';
+export const APIDOMAIN = 'https://mrmolanodev.site';
 export const APIDOMAIN_VERSION = '/api/v1';
